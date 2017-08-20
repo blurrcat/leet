f87d96ae8753a1a0f904bb8d7a9c0ae20ab2474a
blurrcat <blurrcat@gmail.com>
2017-08-20 13:38:37 +0800

    274 h-index

--- leet/problems/template.py
+++ leet/problems/template.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+
+    >>> s = Solution()
+
 """
-import pytest
 
 
 class Solution(object):
 
     def run(self, *args):
         pass
-
-
-TESTCASES = [
-    [[None], None]
-]
-
-
-@pytest.mark.parametrize('args,expected', TESTCASES)
-def test(args, expected):
-    actual = Solution().run(*args)
-    assert actual == expected
