78684010d0cb731df1fec42a835352cd6daa394a
blurrcat <blurrcat@gmail.com>
2017-09-02 12:19:36 +0800

    nlogk solution

--- leet/problems/23_merge_k_sorted_lists.py
+++ leet/problems/23_merge_k_sorted_lists.py
@@ -34,6 +34,34 @@ class ListNode(object):
 
 
 class Solution(object):
+    """
+    In the simple case where there are 2 sorted lists to be merged,
+    it is trivial to merge them in O(n), where n is the total number of
+    elements in the 2 lists. We denote this operation as `merge2(l1, l2)`.
+
+    Consider there are more than 2 sorted lists. One plausible solution is to
+    reduce the lists with `merge2`, like `reduce(merge2, lists)`, or more
+    specifically:
+
+        merged = []
+        for list in lists:
+            merged = merge2(merged, list)
+
+    The time complexity is O(n * k), where k is the number of lists. Note
+    that in each iteration, `list` is processed with all elements merged so
+    far, which is a waste. Observe that merging k lists can be broken into
+    k/2 `merge2` subproblems. By applying this recursively, we come to a
+    divide and conquer solution:
+
+        while len(lists) > 1:
+            lists = [merge2(*every_2_lists) for every_2_lists in lists]
+        return lists[0]
+
+    In each iteration of the while loop, it takes O(n) for all the `merge2`
+    operations. Each loop reduce the number of lists roughly by 1/2. Summing
+    this up, the time complexity of the solution is O(n * log(k, 2)).
+    """
+
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
@@ -43,40 +71,37 @@ class Solution(object):
         lists = [l for l in lists if l]
         if not lists:
             return None
-        while True:
+        while len(lists) > 1:
             merged = []
             for i in xrange(0, len(lists), 2):
                 if i + 1 < len(lists):
                     # merge 2 lists
                     a = lists[i]
                     b = lists[i + 1]
-                    # print 'merging %s and %s' % (a.to_list(), b.to_list())
-                    if a.val < b.val:
-                        root = ListNode(a.val)
-                        a = a.next
-                    else:
-                        root = ListNode(b.val)
-                        b = b.next
-                    parent = root
+                    # insert values from b to a, where a is the list whose
+                    # head is smaller
+                    if b.val < a.val:
+                        a, b = b, a
+                    head = prev = a
+                    a = a.next
                     while a and b:
                         if a.val < b.val:
-                            parent.next = ListNode(a.val)
+                            prev = a
                             a = a.next
                         else:
-                            parent.next = ListNode(b.val)
+                            # insert b between prev and a
+                            tmp = b
                             b = b.next
-                        parent = parent.next
-                    if a:
-                        parent.next = a
-                    elif b:
-                        parent.next = b
-                    merged.append(root)
+                            prev.next = tmp
+                            tmp.next = a
+                            prev = tmp
+                    if b:
+                        prev.next = b
+                    merged.append(head)
                 else:  # one single list left
                     merged.append(lists[i])
-
-            if len(merged) == 1:
-                return merged[0]
             lists = merged
+        return lists[0]
 
 
 @pytest.mark.parametrize('lists,expected', [
@@ -89,13 +114,19 @@ class Solution(object):
     ],
     # simple case: merge 2 lists
     [
-        [[1, 3], [2, 4]], [1, 2, 3, 4]
+        [[1, 3], [2, 4]], [1, 2, 3, 4],
     ],
     [
-        [[1, 3], [2, 4, 5, 6]], [1, 2, 3, 4, 5, 6]
+        [[2, 4], [1, 3]], [1, 2, 3, 4],
     ],
     [
-        [[1, 3, 5, 6], [2, 4]], [1, 2, 3, 4, 5, 6]
+        [[1, 3], [2, 4, 5, 6]], [1, 2, 3, 4, 5, 6],
+    ],
+    [
+        [[1, 3, 5, 6], [2, 4]], [1, 2, 3, 4, 5, 6],
+    ],
+    [
+        [[1, 2, 2], [1, 1, 2]], [1, 1, 1, 2, 2, 2],
     ],
     # merge more than 2 lists
     [
