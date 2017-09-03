8fd5ba76e12e1af9c4545a60dbe18e93a1fe26f1
blurrcat <blurrcat@gmail.com>
2017-09-03 12:15:06 +0800

    670 max swap

---
+++ leet/problems/670_max_swap.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Given a non-negative integer, you could swap two digits at most once to get
+the maximum valued number. Return the maximum valued number you could get.
+
+    >>> s = Solution()
+    >>> s.maximumSwap(1)
+    1
+    >>> s.maximumSwap(10)
+    10
+    >>> s.maximumSwap(12)
+    21
+    >>> s.maximumSwap(2736)
+    7236
+    >>> s.maximumSwap(9973)
+    9973
+    >>> s.maximumSwap(9973)
+    9973
+    >>> s.maximumSwap(856)
+    865
+    >>> s.maximumSwap(88457)
+    88754
+
+"""
+
+
+class Solution(object):
+    def maximumSwap(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num < 10:
+            return num
+        digits = []  # low -> high
+        while num > 0:
+            digits.append(num % 10)
+            num = num // 10
+        # the index of the largest digit from digits[0] to digits[i]
+        largests = [0]
+        for i in xrange(1, len(digits)):
+            idx = largests[-1]
+            if digits[i] > digits[idx]:
+                largests.append(i)
+            else:
+                largests.append(idx)
+        for i in xrange(len(digits) - 1, -1, -1):
+            idx = largests[i]
+            if digits[idx] > digits[i]:
+                digits[idx], digits[i] = digits[i], digits[idx]
+                break
+        return sum(10 ** i * digit for i, digit in enumerate(digits))
